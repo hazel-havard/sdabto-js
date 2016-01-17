fbd1992b80fd5e9a6076a3efcde2975f915277c1
Isaac Havard <ihavard@gmail.com>
2016-01-16 18:05:22 -0800

    Add further Character functions

--- scripts/character.js
+++ scripts/character.js
@@ -72,7 +72,7 @@ function Character() {
     changeStage: function(stage) {
       messages = [];
 
-      if(this.diseaseStage.timeWarp !== undefined
+      if(this.diseaseStage.timeWarp
          && stage == this.diseaseStage.nextStage) {
         message = ' months pass ';
         if(this.timeWarp == 1) {
@@ -85,7 +85,7 @@ function Character() {
         this.hoursPlayed += 24 * 30 * this.diseaseStage.timeWarp;
       }
 
-      if(this.diseaseStage.exitMessage !== undefined) {
+      if(this.diseaseStage.exitMessage) {
         messages.push(this.diseaseStage.exitMessage);
       }
 
@@ -116,7 +116,7 @@ function Character() {
         this.diseaseDays += 1;
 
         if(this.diseaseDays >= this.diseaseStage.length) {
-          if(this.diseaseStage.nextStage === undefined) {
+          if(!this.diseaseStage.nextStage) {
             this.dead = true;
             messages.push('You have committed suicide');
             return messages;
@@ -135,7 +135,7 @@ function Character() {
       this.lastSleep += hours;
       this.hoursPlayed += hours;
 
-      if(this.diseaseStage.effect !== undefined
+      if(this.diseaseStage.effect
          && Math.random() < SIDE_EFFECT_FREQ * hours) {
         messages.push(this.diseaseStage.effect.message);
       }
@@ -149,6 +149,158 @@ function Character() {
         this.dead = true;
       }
 
+      return messages;
+    },
+
+    displayMood: function() {
+      var mood = this.mood;
+
+      if(this.lastMeal > MEAL_INTERVAL) {
+        mood -= Math.min(10 * (this.lastMeal - MEAL_INTERVAL), 30);
+      }
+      if(this.lastExercise > EXERCISE_INTERVAL) {
+        mood -= Math.min(5 * (this.lastExercise - EXERCISE_INTERVAL), 20);
+      }
+      if(this.lastSocial > SOCIAL_INTERVAL) {
+        mood -= Math.min(5 * (this.lastExercise - EXERCISE_INTERVAL), 20);
+      }
+      if(this.lastCleaned > CLEANING_INTERVAL) {
+        mood -= 5;
+      }
+
+      if(mood < 0) {
+        mood = 0;
+      } else if(mood > this.diseaseStage.cap) {
+        mood = this.diseaseStage.cap;
+      }
+
+      return mood;
+    },
+
+    displayEnergy: function() {
+      var energy = this.energy;
+
+      if(this.lastMeal > MEAL_INTERVAL) {
+        energy -= Math.min(10 * (this.lastMeal - MEAL_INTERVAL), 30);
+      }
+      if(this.lastSleep > SLEEP_INTERVAL) {
+        energy -= Math.min(5 * (this.lastSleep - SLEEP_INTERVAL), 20);
+      }
+      if(this.lastExercise > EXERCISE_INTERVAL) {
+        energy -= Math.min(5 * (this.lastExercise - EXERCISE_INTERVAL), 20);
+      }
+      if(this.diseaseStage.effect == LOW_ENERGY) {
+        energy -= this.diseaseStage.effect.penalty;
+      }
+
+      if(energy < 0) {
+        energy = 0;
+      } else if(energy > this.diseaseStage.cap) {
+        energy = this.diseaseStage.cap;
+      }
+
+      return energy;
+    },
+
+    clean: function() {
+      var messages = this.addHours(1);
+      this.energy -= 5;
+      this.lastCleaned = 0;
+      return messages;
+    },
+
+    work: function(hours) {
+      var messages = this.addHours(hours);
+      var wages = 10 * hours;
+      if(this.diseaseStage.wageMultiplier) {
+        wages *= this.diseaseStage.wageMultiplier;
+      }
+      this.money += wages;
+      this.energy -= 5 * hours;
+      this.mood -= 5 * hours;
+      return messages;
+    },
+
+    sleep: function(hours) {
+      var messages = this.addHours(hours);
+
+      if(hours < 4) {
+        return messages;
+      } else if (hours > 8) {
+        hours = 8;
+      }
+
+      this.lastSleep = 0;
+
+      if(this.diseaseStage.sleepEnergy) {
+        this.energy = this.diseaseStage.sleepEnergy;
+        return messages;
+      }
+
+      this.energy = (10 * hours);
+      if(hours > 6) {
+        this.energy += 20;
+      }
+
+      return messages;
+    },
+
+    eat: function() {
+      var messages = this.addHours(1);
+      this.lastMeal = 0;
+      if(!this.diseaseStage.freeMeals) {
+        this.groceries -= 1;
+      }
+      return messages;
+    },
+
+    exercise: function() {
+      var messages = this.addHours(1);
+      this.lastExercise = 0;
+      this.mood += 5;
+      this.energy -= 5;
+      return messages;
+    },
+
+    shopping: function() {
+      var messages = this.addHours(1);
+      this.money -= GROCERIES;
+      this.groceries += 21;
+      if(this.groceries > 42) {
+        messages.push('There is no more room in your fridge.');
+        this.groceries = 42;
+      }
+      return messages;
+    },
+
+    game: function(hours) {
+      var messages = this.addHours(hours);
+      var dailyCap = 4;
+      if(this.diseaseStage.gamingCap) {
+        dailyCap = this.diseaseStage.gamingCap;
+      }
+      hours = Math.max(0, Math.min(hours, dailyCap - this.hoursGamed));
+      this.hoursGamed += hours;
+      this.mood += 5 * hours;
+      return messages;
+    },
+
+    socialize: function(hours) {
+      var messages = this.addHours(hours);
+      this.money -= 10 * hours;
+      this.energy -= 5 * hours;
+      var dailyCap = 3;
+      if(this.diseaseStage.socializingCap) {
+        dailyCap = this.diseaseStage.socializingCap;
+      }
+      hours = Math.max(0, Math.min(hours, dailyCap - this.hoursSocialized));
+      this.hoursSocialized += hours;
+      var moodBonus = 10 * hours;
+      if(this.diseaseStage.socializingMultiplier) {
+        moodBonus *= this.diseaseStage.socializingMultiplier;
+      }
+      this.mood += moodBonus;
+      this.lastSocial = 0;
       return messages;
     }
   };
